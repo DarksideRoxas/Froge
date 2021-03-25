a888977c9b48e1a470fc13b401760a59e93e60bd
Sans <roxasfett@gmail.com>
2021-03-25 13:45:31 -0400

    Tutorial 4 Commits, 15 minutes in

--- Server/index.js
+++ Server/index.js
@@ -36,16 +36,92 @@ app.set('views',viewsPath);
 // Routes
 
 app.get('/', function(req, res) {
-    console.log(req.session);
+    console.log(req.session)
     res.render('index', {data: req.session});
 });
 
-app.get('/Frogs', function(req, res) {
-    res.render('Frogs', {data: req.session});
+app.get('/famous', function(req, res) {
+    res.render('famous', {data: req.session});
+});
+
+app.get('/blog/', async (req, res)=>{
+    var posts = await BlogPost.find({}, (error, result) => {
+        if(error) {
+            console.log(error);
+            res.sendStatus(500);
+        }
+        console.log(result);
+        res.render('blog', {data: req.session, postset: result});
+    });
+
+});
+
+app.get('/blog/write/', (req, res)=>{
+    res.render('writing', {data: req.session, draft: {}});
+});
+
+app.get('/blog/:id/', (req,res) => {
+    var searchID = req.params.id;
+    BlogPost.findById(searchID, (error, result)=>{
+        if(error) {
+            console.log(error);
+            res.redirect('/blog/');
+        }
+        else if(!result) {
+            res.status(404);
+        }
+        else {
+            res.render('entry',{data: req.session, entry: result});
+        }
+    })
+});
+
+app.post('/blog/writepost', async (req, res)=>{
+    console.log(req.body);
+    try {
+        let newPost = new BlogPost(req.body);
+        await newPost.save();
+        res.redirect('/blog/');
+    }
+    catch(e) {
+        res.redirect('/blog/write/');
+    }
 });
 
 app.post('/welcome', (req, res) => {
-    console.log(req.body);
     req.session.username=req.body.nombre;
-    res.status(Success);
+    res.send('SUCCESS');
+});
+
+app.get('/blog/:id/edit', (req,res)=>{
+    BlogPost.findById(req.params.id, (error, result)=>{
+        if(error) res.redirect('/blog/');
+        else if(!result) res.redirect('/blog/');
+        else res.render('writing', {data: req.session, draft: result} );
+    });
+});
+
+app.post('/blog/:id/edit', (req, res)=>{
+    BlogPost.findById(req.params.id, (error, result)=>{
+        if(error) {
+            console.log(error);
+            res.status(500);
+        }
+        else if (result) {
+            result.title = req.body.title;
+            result.body = req.body.body;
+            result.save();
+            res.redirect(path.join('/blog/', req.params.id));
+        }
+        else res.redirect('/blog/');
+    });
+});
+
+app.get('/blog/:id/delete', (req, res)=>{
+    BlogPost.deleteOne({_id: req.params.id}, (error, result)=>{
+        if(error) {
+            console.log(error);
+        }
+        res.redirect('/blog/');
+    });
 });
